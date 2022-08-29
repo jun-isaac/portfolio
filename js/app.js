dc4e4dd4d25837aa441d0f060ddc2b9433ec0423
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-08-29 15:07:50 +0900

    about me 모바일 완성

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-let pageable = new Pageable('#aboutme');
+$(document).ready(function () {
+  
+  $('#fullpage').fullpage({
+    //options here
+    responsiveWidth: 500, // fullpage 해제 분기점
+		scrollOverflow: true, // 부분 스크롤 영역 지정(.fp-auto-height-responsive)
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixPage'],
+  });
 
-// console.log(pageable);
+});
+
+AOS.init();
