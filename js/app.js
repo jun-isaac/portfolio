a1435a5146be0500a9705e0b2a3f64a61dcf874b
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-09-06 10:51:45 +0900

    페이지 인식시 차트 움직임

--- js/app.js
+++ js/app.js
@@ -6,8 +6,59 @@ $(document).ready(function () {
     responsiveWidth: 500, // fullpage 해제 분기점
     scrollOverflow: true, // 부분 스크롤 영역 지정(.fp-auto-height-responsive)
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+
+    // 이벤트 methods
+    // 페이지 진입시(액션)
+    afterLoad: function (anchorLink, index) {
+      // console.log(index);
+
+      // 3번째 페이지에 들어오면
+      if (index === 3) {
+        replay();
+      } else {
+
+      }
+    },
+
   });
 });
 
 // aos 라이브러리
 AOS.init();
+
+
+// ability 차트 영역
+const chart1 = document.querySelector('.doughnut1');
+const chart2 = document.querySelector('.doughnut2');
+const chart3 = document.querySelector('.doughnut3');
+const chart4 = document.querySelector('.doughnut4');
+const chart5 = document.querySelector('.doughnut5');
+const chart6 = document.querySelector('.doughnut6');
+const chart7 = document.querySelector('.doughnut7');
+
+const makeChart = (percent, classname, color) => {
+  let i = 1;
+  let chartFn = setInterval(function () {
+    if (i < percent) {
+      colorFn(i, classname, color);
+      i++;
+    } else {
+      clearInterval(chartFn);
+    }
+  }, 10);
+}
+
+const colorFn = (i, classname, color) => {
+  classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #1E3799 " + i + "% 100%)";
+}
+
+
+const replay = () => {
+  makeChart(70, chart1, '#ffffff');
+  makeChart(68, chart2, '#ffffff');
+  makeChart(84, chart3, '#ffffff');
+  makeChart(75, chart4, '#ffffff');
+  makeChart(87, chart5, '#ffffff');
+  makeChart(85, chart6, '#ffffff');
+  makeChart(80, chart7, '#ffffff');
+}
